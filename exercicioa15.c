09e3189c58c0f7a14d61eabe4a37d656c9f24241
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-27 11:15:33 -0300

    Create exercicioa15.c

---
+++ exercicioa15.c
@@ -0,0 +1,11 @@
+//Leia um angulo em radianos e apresente-o convertido em graus. A formula de conversao˜
+//e: ´ G = R ∗ 180/π, sendo G o angulo em graus e ˆ R em radianos e π = 3.14.
+#include <stdio.h>
+int main(){
+    float g,r;
+    float pi=3.14;
+    printf("Seu angulo possui quantos radianos");
+    scanf("%f",&r);
+    g=r*180/pi;
+    printf("Essa quantidade de radianos equivale a %f graus",g);
+}
