f0de354d9ce66e3255af223a1c7c66a6e587049e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-27 10:45:49 -0300

    Create exercicioa11.cc

---
+++ exercicioa11.cc
@@ -0,0 +1,11 @@
+//Leia uma velocidade em m/s (metros por segundo) e apresente-a convertida em km/h
+//(quilometros por hora). A f ˆ ormula de convers ´ ao˜ e: ´ K = M ∗ 3.6, sendo K a velocidade
+//em km/h e M em m/s.
+#include <stdio.h>
+int main(){
+    float m,km;
+    printf("Digite a velocidade em m/s ");
+    scanf("%f",&m);
+    km=m*3.6;
+    printf("A velocidade em km por hora equivale a %f",km);
+}
