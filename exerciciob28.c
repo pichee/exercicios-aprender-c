8b3d9719a4947c34bbf965ba2c474ef53a70598d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-27 20:31:40 -0300

    Create exerciciob28.c

---
+++ exerciciob28.c
@@ -0,0 +1,15 @@
+//Read  three values and show the  result,the addition from squares from
+//three values read.
+#include <stdio.h>
+    int main(){
+        int i=0;
+        float n,a;
+            while (i<3){
+                printf("put the number")/
+                scanf("%f",&n);
+                n=n*n;
+                a=a+n;
+                i++;
+            }
+            printf("The result of squares three number is %.2f",a);
+    }
