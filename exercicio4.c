af78c659bfac2ea83234246962ff420eab332d19
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-27 16:34:18 -0300

    Update exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -8,6 +8,6 @@ main ()
   printf ("Digite um número");
   scanf("%f",&n);
   n=n*n;
-  printf("O quadrado desse número é %2.f",n);    
+  printf("O quadrado desse número é %.2f",n);    
   return 0;
 }
