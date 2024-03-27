c08ba7f1291bd7383980999e890f1023acc45180
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-27 20:40:52 -0300

    Create exerciciob29.c

---
+++ exerciciob29.c
@@ -0,0 +1,16 @@
+//read four numbers, calculate the Arithmetic Mean and showing the result.
+#include <stdio.h>
+int main(){
+    int i=0;
+    int a,n;
+    while (i<4)
+    {
+        printf("type a entire number");
+        scanf("%i",&n);
+        a = n+a;
+        i++;
+    }
+    a=a/4;
+    printf("The Arithmetic mean is equal %.2f",a);
+    
+}
