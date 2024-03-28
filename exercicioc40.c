b714ba600a78794ecad98a30c724d34973d93e6d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-28 15:27:04 -0300

    Update exercicioc40.c

--- exercicioc40.c
+++ exercicioc40.c
@@ -1,1 +1,12 @@
-
+//a company hire a plumber  for  R$ 30,00 each day.Make a program what request
+//the number the eork days for plumberr and show amount  lıquid he receved´
+//know 8% is discont because the tax.
+#include <stdio.h>
+    int main(){
+        float d;
+        printf("How many days the plumber work? ");
+        scanf("%f",&d);
+        d=d*30;
+        d=d-(d*0.08);
+        printf("He going receive approximately  %.2f Reais",d);
+    }
