8321b303ed5cf5ad78bc1d16d123382119721886
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-28 11:25:10 -0300

    Create exerciciob33.c

---
+++ exerciciob33.c
@@ -0,0 +1,9 @@
+//Read the side size your square and show with result your zone.
+#include <stdio.h>
+    int main(){
+        float l;
+        printf("How is side size your square? ");
+        scanf("%f",&l);
+        l=l*l;
+        printf("Your zone is igual %.2f",l);
+    }
