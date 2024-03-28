1477cb35d1e746bf11abad1786918083b96e1beb
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-28 11:11:40 -0300

    Create exerciciob31.c

---
+++ exerciciob31.c
@@ -0,0 +1,10 @@
+//read  a whole number and showing your predecessor and successor.
+#include <stdio.h>
+    int main(){
+        int n,n1,n2;
+        printf("type a whole number ");
+        scanf("%i",&n);
+        n1=n-1;
+        n2=n+1;
+        printf("The predecessor of this number is %i and successor is %i",n1,n2);
+    }
