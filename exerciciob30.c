7529e95fec6911ed28049f54b0d65ab091f8ed6b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-28 11:06:27 -0300

    Create exerciciob30.c

---
+++ exerciciob30.c
@@ -0,0 +1,12 @@
+//Read a value in real and the dolar exchance rate.Next,showing  the value in   corresponding
+//in dolar.
+#include <stdio.h>
+    int main(){
+        float r,d,c;
+        printf("How many reals do you have ");
+        scanf("%f",&r);
+        printf("How is current dolar exchance rate ");
+        scanf("%f",&d);
+        c=r/d;
+        printf("You can buy %.2f dollars with your reals",c);
+    }
