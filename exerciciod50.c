4c91b114b4efefb8379c6d7a17733da5f4b948a7
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-29 19:26:34 -0300

    Create exerciciod50.c

---
+++ exerciciod50.c
@@ -0,0 +1,9 @@
+//Make a program that calculetes the yer of the birthday the one person from your years and the and from the current year a partir de
+#include <stdio.h>
+	int main(){
+		int i;
+		printf("How many years do you have");
+		scanf("%i",&i);
+		i=2024-i;
+		printf("considering we be in  2024 you were born in %i",i);
+	}
