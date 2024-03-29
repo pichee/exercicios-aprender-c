98179cadcbe814bda70a6c705460ec6efded8e5d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-29 11:31:49 -0300

    Create exercicioc45.c

---
+++ exercicioc45.c
@@ -0,0 +1,11 @@
+// Make  a program for converter  a  capital letter in lower letter.Use the 
+//ASCII table  for resolv this problem.
+#include <stdio.h>
+int main(){
+    char m,n;
+    printf("put a capital letter ");
+    scanf("%c",&m);
+    n = (m >= 'A' && m <= 'Z') ? m + 32 : m;
+    printf("This letter is  %c in lower letter",n);
+    
+}
