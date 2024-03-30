df1f4cefb68110e6a8a2ea0b1d8560b2051cf326
Gustavo <76635390+pichee@users.noreply.github.com>
2024-03-30 11:31:40 -0300

    Create exerciciod56.c

---
+++ exerciciod56.c
@@ -0,0 +1,16 @@
+//Read a number if is posivite show your square if is negative show your double
+#include <stdio.h>
+#include <math.h>
+    int main(){
+        float n;
+        printf("Enter a number");
+        scanf("%f",&n);
+        if (n>0){
+            n=sqrt(n);
+            printf("Your square is %.2f",n);
+        }else{
+            n=n*n;
+            printf("the double of this number is %.2f",n);
+            
+        }
+    }
