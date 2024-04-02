6ce5ff910cc1b264ecaeef9b2f84a1a2c9983436
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-02 17:28:38 -0300

    Create exerciciod68.c

---
+++ exerciciod68.c
@@ -0,0 +1,31 @@
+//Using switch,write a program what read a int number that 1 and  7 and  show the day of the
+//week.
+#include <stdio.h>
+int main(){
+    int d;
+    printf("Choose a number that 1 than 7\n ");
+    scanf("%i",&d);
+    switch(d){
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+    }
+}
