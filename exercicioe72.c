6dcc99c61a79defc1e72c691b502dc46d060c4cd
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-03 13:33:36 -0300

    Create exercicioe72.c

---
+++ exercicioe72.c
@@ -0,0 +1,18 @@
+//Make a program show if the number is division by three or five;
+#include <stdio.h>
+    int main(){
+        int n,a,a1;
+        printf("Enter a whole number");
+        scanf("%i",&n);
+        a=n%3;
+        a1=n%5;
+            if(a==0){
+                printf("This number is division by 3");
+                }
+            if(a1==0){
+                printf("This number is division by 5");
+            }
+            if(a==0 && a1==0){
+                printf("This number dont divison by 5 or 3 :<");
+            }
+    }
