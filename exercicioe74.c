5f73e28d2ef7892b41edf8045f875cfaad94ae9a
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-04 13:14:13 -0300

    Create exercicioe74.c

---
+++ exercicioe74.c
@@ -0,0 +1,44 @@
+//Make a frame show 4 options
+//1- addition of two numbers
+//2- difference of two numbers (bigger of the smaller).
+//3- multiplication of two numbers.
+//4-division of two numbers(the  denominador can't be zero).
+#include <stdio.h>
+    int main(){
+        float n,n1;
+        int c;
+        printf("==========\n Chose an operation to make\n 1-addition of two numbers \n 2-difference of two numbers \n 3-multiplication two numbers \n 4-division of two numbers\n==========\nChose:");
+        scanf("%i",&c);
+        printf("Enter a number\n");
+        scanf("%f",&n);
+        printf("Enter a other number\n");
+        scanf("%f",&n1);
+         if(c>4){
+                    printf("Wrong number");
+                    }
+            switch(c){
+               case 1:
+                n=n+n1;
+                printf("The addition is igual %.2f",n);
+                break;
+                case 2:
+                if(n1>n){
+                    n=n1-n;
+                }else{
+                  n=n-n1;  
+                }
+                printf("The difference is igual %.2f",n);
+                break;
+                case 3:
+                n=n*n1;
+                printf("The multiplication is igual %.2f",n);
+                break;
+                case 4:
+                if(n1==0){
+                    printf("The denominator can't be zero");
+                }else{
+                n=n/n1;
+                printf("The division is igual %.2f",n);
+                break;}
+            }
+        }
