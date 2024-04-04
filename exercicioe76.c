cdf4062bf8ece756cdb18e77363acc055bab4ae7
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-04 13:37:42 -0300

    Create exercicioe76.c

---
+++ exercicioe76.c
@@ -0,0 +1,18 @@
+//Check if is a leap year
+#include <stdio.h>
+   int main(){
+        int y,a,a1;
+        printf("Enter a year\n");
+        scanf("%i",&y);
+        a=y;
+        a1=y;
+        y=y%400;
+        a=a%4;
+        a1=a1%100;
+        
+        if(y==0 || a==0 && a1!=0){
+        printf("This is a leap year");
+        }else{
+            printf("This is not a leap year");
+        }}
+    
