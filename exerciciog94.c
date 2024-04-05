621d1b0513ea90d4d5dbb2b6e4fbef762c066851
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-05 18:25:20 -0300

    Create exerciciog94.c

---
+++ exerciciog94.c
@@ -0,0 +1,23 @@
+//Just make the imc
+#include <stdio.h>
+    int main(){
+        float t,w;
+        printf("What is your tall?\n");
+        scanf("%f",&t);
+        printf("What is your weight?\n");
+        scanf("%f",&w);
+        t=w/(t*t);
+        if(t<=18.5){
+            printf("under weight");
+        }else if(t>18.5&&t<=24.9){
+            printf("healthy");
+        }else if(t>25&&t<=29.9){
+            printf("excess weight");
+        }else if(t>30&&t<=34.9){
+            printf("obesity dee grade 1");
+        }else if(t>35&&t<=39.9){
+            printf("obesity dee grade 2");
+        }else{
+            printf("obesity dee grade 3");
+        }
+    }
