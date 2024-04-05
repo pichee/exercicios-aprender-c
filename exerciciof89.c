40834a39889321dd3872b0693541686e04c71330
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-05 15:42:13 -0300

    Create exerciciof89.c

---
+++ exerciciof89.c
@@ -0,0 +1,29 @@
+//Write a program that, given the value of the sale, prints the commission that should be paid to the seller. 
+//To calculate the commission, consider the table below:
+#include <stdio.h>
+    int main(){
+        float s;
+        printf("How much did you sell\n");
+        scanf("%f",&s);
+        if(s>=100000){
+            s=700+(s*0.16);
+            printf("You will receb %.2f",s);
+        }else if(s<100000&&s>=80000){
+            s=650+(s*0.14);
+            printf("You will receb %.2f",s);
+        }else if(s<80000&&s>=60000){
+            s=600+(s*0.14);
+            printf("You will receb %.2f",s);
+        }else if(s<60000&&s>=40000){
+            s=550+(s*0.14);
+            printf("You will receb %.2f",s);
+        }else if(s<40000&&s>=20000){
+            s=500+(s*0.14);
+            printf("You will receb %.2f",s);
+        }else{
+            s=400+(s*0.14);
+            printf("You will receb %.2f",s);
+        }
+            
+        }
+        
