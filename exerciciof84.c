eccc41f89ba06d2f53ac0edafc53ad4cb1c9fcd0
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-05 11:10:30 -0300

    Create exerciciof84.c

---
+++ exerciciof84.c
@@ -0,0 +1,44 @@
+//Make a program that classffy a person base your tall and your weight.
+//Using the frame for classffy
+//Obs:I don't know you put the frame in c :/
+
+
+#include <stdio.h>
+
+int main()
+{
+    float p,t;
+    printf("What is you tall?\n");
+    scanf("%f",&t);
+    printf("What is you weight?\n");
+    scanf("%f",&p);
+    
+    
+    
+    if (t < 1.20 && p < 60) {
+        printf("Your classification is A");
+    }
+    else if (t < 1.20 && p >= 60 && p <= 90) {
+        printf("Your classification is D");
+    }
+    else if (t < 1.20 && p > 90) {
+        printf("Your classification is F");
+    }
+    else if (t >= 1.20 && t < 1.70 && p < 60) {
+        printf("Your classification is B");
+    }
+    else if (t >= 1.20 && t < 1.70 && p >= 60 && p <= 90) {
+        printf("Your classification is E");
+    }
+    else if (t >= 1.70 && p < 60) {
+        printf("Your classification is C");
+    }
+    else if (t >= 1.70 && p >= 60 && p <= 90) {
+        printf("Your classification is F");
+    }
+    else if (t >= 1.70 && p > 90) {
+        printf("Your classification is G");
+    }
+    else {
+        printf("Your classification is H");
+    }}
