5232b811d1400bfcf0f64e82f7ed4f12c63129bc
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-04 22:12:19 -0300

    Create exerciciof83.c

---
+++ exerciciof83.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+    int main(){
+        float n,n1,n2;
+        printf("Enter a number\n");
+        scanf("%f",&n);
+         printf("Enter a number\n");
+        scanf("%f",&n1);
+         printf("Enter a number\n");
+        scanf("%f",&n2);
+        if(n1>n&&n>n2){
+            printf("The ascending order is %.2f %.2f %.2f",n1,n,n2);
+        }
+        if(n2>n&&n>n1){
+            printf("The ascending order is %.2f %.2f %.2f",n2,n,n1);
+        }
+        if(n1>n2&&n2>n){
+            printf("The ascending order is %.2f %.2f %.2f",n1,n2,n);
+        }
+        if(n2>n1&&n1>n){
+            printf("The ascending order is %.2f %.2f %.2f",n2,n1,n);
+        }
+        if(n>n1&&n1>n2){
+            printf("The ascending order is %.2f %.2f %.2f",n,n1,n2);
+        }
+        if(n>n2&&n2>n1){
+            printf("The ascending order is %f-%f,-%f",n,n2,n1);
+        }}
