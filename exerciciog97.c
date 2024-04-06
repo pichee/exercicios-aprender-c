ca702fd742659720573147b2b96db5654a56ff83
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-06 10:23:55 -0300

    Create exerciciog97.c

---
+++ exerciciog97.c
@@ -0,0 +1,9 @@
+//Make a program with while and show a desceding order between 10 and 0
+#include <stdio.h>
+int main(){
+    int a=10;
+    while(a>-1){
+        printf("\n%i",a);
+        a--;
+    }
+}
