3643d30df58d96368b628864b346e106128ef95b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-06 09:35:30 -0300

    Create exerciciog95.c

---
+++ exerciciog95.c
@@ -0,0 +1,10 @@
+//Show the five firsts multiple the three
+#include <stdio.h>
+    int main(){
+        int a,t;
+        while(a<=4){
+            t=t+3;
+            printf("\n%i",t);
+            a++;
+        }
+    }
