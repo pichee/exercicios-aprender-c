e9770522333f81f9038f3671c13abd151dd3e7bb
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-06 10:28:46 -0300

    Create exerciciof98.c

---
+++ exerciciof98.c
@@ -0,0 +1,9 @@
+//make a program star in zero and end in one hundred thousand
+#include <stdio.h>
+int main(){
+    int a=0;
+        while(a <=100000 ){
+            printf("\n%i",a);
+            a=a+1000;
+        }
+}
