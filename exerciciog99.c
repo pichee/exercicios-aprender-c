d6005353aca33fc7b2857a6578b4981be06a8682
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-06 11:32:28 -0300

    Create exerciciog99.c

---
+++ exerciciog99.c
@@ -0,0 +1,13 @@
+//make a program the user enter 10 numbers and you show the addition;
+#include <stdio.h>
+int main(){
+    int a=0;
+    int n,d;
+        while(a <=10){
+            printf("Enter a  whole number\n");
+            scanf("%i",&d);
+            n=n+d;
+            a++;
+        }
+        printf("The addition is %i",n);
+}
