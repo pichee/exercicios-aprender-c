4bcd4aaf8222bb4ac2263faecf95dae756f9d2b3
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-08 11:17:05 -0300

    Create exercicioj105.c

---
+++ exercicioj105.c
@@ -0,0 +1,13 @@
+//Make a program show de ascending order of the a number;
+#include <stdio.h>
+int main(){
+    int c=0;
+    int n,a;
+
+    printf("Enter a  whole number\n");
+    scanf("%i",&n);
+    while(a<=n){
+        printf("\n%i",c);
+        c=c+1;
+        a++;
+    }}
