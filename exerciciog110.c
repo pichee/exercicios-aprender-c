771fe62359cbe3ba4af7a83cc73c6d591221a60e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-08 11:56:13 -0300

    Create exerciciog110.c

---
+++ exerciciog110.c
@@ -0,0 +1,13 @@
+//Make a program that read a number and show the addition of all there until the number
+#include <stdio.h>
+    int main(){
+        int c,n,a,n1;
+        printf("Enter a whole number\n");
+        scanf("%i",&n);
+        while(a<=n){
+            c=n1+c;
+            n1=n1+1;
+            a++;
+        }
+        printf("the addition of the numbers until %i is igual %i",n,c);
+    }
