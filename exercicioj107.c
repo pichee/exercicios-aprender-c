75b0c4a871ff1484c9013a2753f6159abb4e137c
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-08 11:23:44 -0300

    Create exercicioj107.c

---
+++ exercicioj107.c
@@ -0,0 +1,18 @@
+//Make a program read the a numbers and show the numbers pair until n in order crescent;
+#include <stdio.h>
+int main(){
+    int c;
+    int n,a,a1;
+
+    printf("Enter a  whole number pair\n");
+    scanf("%i",&n);
+    c=n%2;
+    if(c==0){
+    while(a<=n){
+        printf("\n%i",a1);
+        a1=a1+2;
+        a++;
+        a++;
+    }}else{
+        printf("ERROR");
+    }}
