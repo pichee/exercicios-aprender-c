8526c6be719643c45985d9103011b735a2b35e55
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-08 11:00:57 -0300

    Create exercicioj101.c

---
+++ exercicioj101.c
@@ -0,0 +1,18 @@
+//make a program that shoe the media and ingoring the negatives numbers  
+#include <stdio.h>
+    int main(){
+        int n,a,a1;
+        float d,t;
+        while(a<10){
+        printf("Enter a while number\n");
+        scanf("%i",&n);
+        if(n>=0){
+            d=d+n;
+            a1=a1+1;
+        }
+        a++;
+        t=d/a1;
+    }
+    
+    printf("Your media is %.2f",t);
+    }
