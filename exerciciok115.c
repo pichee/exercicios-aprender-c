f7d40360db948602e88c19fc2a4fb1428efb6c8e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-09 17:18:56 -0300

    Create exerciciok115.c

---
+++ exerciciok115.c
@@ -0,0 +1,14 @@
+//Show de divisors the a number
+#include <stdio.h>
+int main(){
+    int n,a=1,d;
+    printf("Enter a number\n");
+    scanf("%i",&n);
+    while(a<=n){
+        d=n%a;
+        if(d==0){
+            printf("%i is divisors this number\n",a);
+        }
+        a++;
+    }
+}
