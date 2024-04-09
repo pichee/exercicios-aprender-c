0faeda0a3b4bf78e375b826be09cd228cee6eabe
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-09 18:05:13 -0300

    Create exerciciok117.c

---
+++ exerciciok117.c
@@ -0,0 +1,15 @@
+//Addition divisors 3 and 5 until 1000;
+#include <stdio.h>
+int main(){
+    int n=1000,a=1,d,s=0,d1;
+    while(a<n){
+        d=a%3;
+        d1=a%5;
+        if(d==0||d1==0){
+            s=s+a;
+        }
+        
+        a++;
+    }
+    printf("The addition of your divisirors is %i",s);
+}
