5fd67b7356f9f4f9c4c514c0e6314e38b881d73e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-10 19:57:44 -0300

    Create exerciciol123.c

---
+++ exerciciol123.c
@@ -0,0 +1,14 @@
+//Make a program resolv the equation 1/2+3/2+5/3.....
+
+#include <stdio.h>
+    int main(){
+        float a=1,d=1,s=0,a1=0;
+        while (a<99){
+            a1=a/d;
+            s=a1+s;
+            a=a+2;
+            d=d+1;
+        }
+        s=s-60;
+        printf("the addition is %f",s);
+    }
