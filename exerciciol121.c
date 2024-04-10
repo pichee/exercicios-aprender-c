b342fd6bb5102d3d206efc95ef66eb41a33b63bc
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-10 18:09:02 -0300

    Create exerciciol121.c

---
+++ exerciciol121.c
@@ -0,0 +1,16 @@
+//addition the numbers the formula S = 0 + 1/2! + 2/4! + 3/6! + ... until the five
+#include <stdio.h>
+int main(){
+float n,s;
+    n=0.5;
+    s=s+n;
+    n=0.083;
+    s=s+n;
+    n=0.0041;
+    s=s+n;
+    n=0.000099;
+    s=s+n;
+    n=0.0000013;
+    s=s+n;
+    printf("the result is %f",s);
+}
