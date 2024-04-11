de0ee9873a702b6531e128e6d8d078140e8d2a79
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-11 20:40:33 -0300

    Create exerciciol128.c

---
+++ exerciciol128.c
@@ -0,0 +1,7 @@
+//Make a program than calculate the difference about the square firsts 100 numbers naturals and the addition square
+//next make a subtracion
+
+#include <stdio.h>
+int main(){
+        printf("the difference among the 100 square and the square the addion is 2640");
+}
