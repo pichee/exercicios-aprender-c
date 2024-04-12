7212a787acc7a82fe36224bf9e0086c88fd54def
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-12 07:37:46 -0700

    Atualizar o

---
+++ exerciciom130.c
@@ -0,0 +1,4 @@
+//
+// foi por soma e produtos e fiz no papel
+//a =200, b = 375, c = 425.
+//Questão muito difícil só faria de novo se me pagassem e pra piorar só consegui por que recebi auxílio do chatgpt
