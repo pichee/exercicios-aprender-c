3de5a9c72c01229d40b3f2141e534b485ad2aa99
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-12 07:43:45 -0700

    Atualizar o exerciciom130.c

--- exerciciom130.c
+++ exerciciom130.c
@@ -1,4 +1,5 @@
-//
+//Fac¸a um programa que calcule o terno pitagorico 
+//a, b, c, para o qual a+b+c = 1000.
 // foi por soma e produtos e fiz no papel
 //a =200, b = 375, c = 425.
 //Questão muito difícil só faria de novo se me pagassem e pra piorar só consegui por que recebi auxílio do chatgpt
