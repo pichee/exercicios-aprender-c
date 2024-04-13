f31124648637b1ea510502d1f7d2fc13b91b6247
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-12 21:53:26 -0300

    Create exerciciom131.c

---
+++ exerciciom131.c
@@ -0,0 +1,13 @@
+//calculate the other value the triangule,know the height and the base
+#include <stdio.h>
+       int main(){
+                int h,b,s;
+                printf("Enter  the height of  triangule");
+                scanf("%i",&h);
+                printf("Enter the base the triangule");
+                scanf("%i",&b);
+                h=h*h;
+                b=b*b;
+                s=h+b;
+printf("the other slide should be %i",s);
+       }
