449ae384946b6d29aab4b4a4f8a7ddff37d1acd9
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-14 16:50:27 -0300

    Create exercicion143.c

---
+++ exercicion143.c
@@ -0,0 +1,34 @@
+//Write a program that receives as input the value of the withdrawal made by the customer
+//from a bank and returns how many notes of each denomination will be required to fulfill the
+//withdrawal with the smallest possible number of notes. Notes of 100, 50,
+//20, 10, 5, 2, and 1 real will be used.
+int main() {
+    int valor, notas100, notas50, notas20, notas10, notas5, notas2, notas1;
+
+    printf("Digite o valor do saque: ");
+    scanf("%d", &valor);
+
+    notas100 = valor / 100;
+    valor %= 100;
+    notas50 = valor / 50;
+    valor %= 50;
+    notas20 = valor / 20;
+    valor %= 20;
+    notas10 = valor / 10;
+    valor %= 10;
+    notas5 = valor / 5;
+    valor %= 5;
+    notas2 = valor / 2;
+    valor %= 2;
+    notas1 = valor;
+
+    printf("Notas de 100: %d\n", notas100);
+    printf("Notas de 50: %d\n", notas50);
+    printf("Notas de 20: %d\n", notas20);
+    printf("Notas de 10: %d\n", notas10);
+    printf("Notas de 5: %d\n", notas5);
+    printf("Notas de 2: %d\n", notas2);
+    printf("Notas de 1: %d\n", notas1);
+
+    return 0;
+}
