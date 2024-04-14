b29cc2bf2acd9193577e6d2c2984e6309c970a1e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-14 09:49:17 -0300

    Create exerciciom139.c

---
+++ exerciciom139.c
@@ -0,0 +1,20 @@
+// show the fibonacci sequence until 4000000
+#include <stdio.h>
+int main(){
+    int a=0,n,n1=1,g,c=1;
+    while (a<4000000)
+    {
+        printf("%i ",a);
+        if(c==1){
+            printf("%i ",c);
+           c++; 
+        }
+        g=n+n1;
+        n=n1;
+        n1=g;
+        a=g;
+
+
+    }
+    
+}
