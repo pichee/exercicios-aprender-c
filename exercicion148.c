05150070e5c4cc98c4b7bfa1df85cae651ec54e8
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-15 16:24:46 -0300

    Create exercicion148.c

---
+++ exercicion148.c
@@ -0,0 +1,39 @@
+//The user enter a sequence and you show how many prime numbers is
+#include <stdio.h>
+
+int main() {
+    int n,n1,tro, s = 0, c = 0, i = 2, j, p;
+    
+    printf("Enter a number:\n");
+    scanf("%i", &n);
+    printf("Enter other number\n");
+    scanf("%i", &n1);
+    if(n>n1){
+        tro=n1;
+        n1=n;
+        n=tro;
+        
+    }
+    while (n < n1) {
+        p = 1;
+      
+        for (j = 2; j * j <= n; j++) {
+            if (n % j == 0) {
+                p = 0; 
+                break;
+            }
+        }
+
+        if (p) {
+            c++; 
+        }
+        if(n==1){
+            c--;
+        }
+        n++;
+    }
+
+    printf("The sum of the first %i",c);
+
+    return 0;
+}
