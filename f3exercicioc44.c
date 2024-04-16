e24a3def4e0065d9ba1959bfb16eb9f76b4389c8
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:53:55 -0300

    Update and rename exerciciom135.c to f3exercicioc44.c

--- f3exercicioc44.c
+++ f3exercicioc44.c
@@ -1,3 +1,7 @@
+//Leia um numero positivo do usuario, entao, calcule e imprima a sequencia Fibonacci ate
+//o primeiro numero superior ao numero lido. Exemplo: se o usuario informou o numero
+//30, a sequencia a ser impressa ser a 0 1 1 2 3 5 8 13 21 34.
+
 //Reead a number and show the sequencee the fibonaci under the number exemplo
 //30
 // 0 1 1 2 3 5 8 13 21 34.
