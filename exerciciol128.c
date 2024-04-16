52646e4aed39bec506ab629792d762e004177871
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:11:47 -0300

    Update and rename exerciciol128.c to f3exercicioc36.c

--- exerciciol128.c
+++
@@ -1,7 +0,0 @@
-//Make a program than calculate the difference about the square firsts 100 numbers naturals and the addition square
-//next make a subtracion
-
-#include <stdio.h>
-int main(){
-        printf("the difference among the 100 square and the square the addion is 2640");
-}
---
+++ f3exercicioc36.c
@@ -0,0 +1,14 @@
+//Faca um programa que calcule a diferenc¸a entre a soma dos quadrados dos primeiros
+//100 numeros naturais e o quadrado da soma. Ex: A soma dos quadrados dos dez pri- ´
+//meiros numeros naturais e,12 + 22 + ... + 102 = 385
+//O quadrado da soma dos dez primeiros numeros naturais  e,
+//(1 + 2 + ... + 10)2 = 552 = 3025
+//A diferenc¸a entre a soma dos quadrados dos dez primeiros numeros naturais e o qua
+//drado da soma e 3025-385 = 2640.
+//Make a program than calculate the difference about the square firsts 100 numbers naturals and the addition square
+//next make a subtracion
+
+#include <stdio.h>
+int main(){
+        printf("the difference among the 100 square and the square the addion is 2640");
+}
