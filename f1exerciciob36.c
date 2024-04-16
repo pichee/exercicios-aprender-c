94c4df9da1631cce4a1d2e0bf42eba6e09458b13
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:29:27 -0300

    Update and rename exerciciob36.c to f1exerciciob36.c

--- f1exerciciob36.c
+++ f1exerciciob36.c
@@ -1,3 +1,6 @@
+//Leia a altura e o raio de um cilindro circular e imprima o volume do cilindro. O volume
+//de um cilindro circular e calculado por meio da seguinte f ormula;V = π ∗ raio2 ∗ altura,
+//onde π = 3.141592.
 //Read the height and the raidios from a  cylinder circule and show the volume from cylinder.The volume
 //the cylinder circule is calculating by means of formulaV = π ∗ raio2 ∗ height,
 //considering π = 3.141592.
