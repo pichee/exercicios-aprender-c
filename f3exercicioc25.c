8309f10661e59ff059f26540ccc92a86060aec36
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:35:59 -0300

    Update and rename exerciciok117.c to f3exercicioc25.c

--- f3exercicioc25.c
+++ f3exercicioc25.c
@@ -1,3 +1,5 @@
+//Faca um programa que some todos os numeros naturais abaixo de 1000 que s ´ ao m ˜ ultiplos ´
+//de 3 ou 5.
 //Addition divisors 3 and 5 until 1000;
 #include <stdio.h>
 int main(){
