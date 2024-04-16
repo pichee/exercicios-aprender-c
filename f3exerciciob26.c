81af0cec879117dd484e6b904fe6f176192b4ed4
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:08:29 -0300

    Update and rename exerciciok118.c to f3exerciciob26.c

--- f3exerciciob26.c
+++ f3exerciciob26.c
@@ -1,3 +1,6 @@
+//Faca um algoritmo que encontre o primeiro multiplo de 11, 13 ou 17 apos um numero
+//dado.
+
 //Make an algorithm that finds the first multiple of 11, 13, or 17 after a given number.
 #include <stdio.h>
 int main(){
