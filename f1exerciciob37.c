2b739cd7709839b7abdfbebe4b0df441dee80a22
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:30:38 -0300

    Update and rename exerciciob37.c to f1exerciciob37.c

--- f1exerciciob37.c
+++ f1exerciciob37.c
@@ -1,3 +1,5 @@
+//Faca um programa que leia o valor de um produto e imprima o valor com desconto, tendo
+//em vista que o desconto foi de 12%
 //Make  a program that read value the a produc and show the value with descont,have
 //a descont from 12%  in cash
 #include <stdio.h>
