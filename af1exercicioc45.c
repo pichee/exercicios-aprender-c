83a6be17a65b07dd0a1dc0b0a1c01315269f1a77
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:36:17 -0300

    Update and rename exercicioc45.c to af1exercicioc45.c

--- af1exercicioc45.c
+++ af1exercicioc45.c
@@ -1,3 +1,6 @@
+//Faca um programa para converter uma letra maiuscula em letra minuscula. Use a tabela
+//ASCII para resolver o problema.
+
 // Make  a program for converter  a  capital letter in lower letter.Use the 
 //ASCII table  for resolv this problem.
 #include <stdio.h>
