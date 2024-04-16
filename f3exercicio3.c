729c1d62fc1cb7ff92c8f17de2df347433fd1aef
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:26:21 -0300

    Update and rename exerciciog97.c to f3exercicio3.c

--- f3exercicio3.c
+++ f3exercicio3.c
@@ -1,3 +1,7 @@
+//Faca um algoritmo utilizando o comando while que mostra uma contagem regressiva
+//na tela, iniciando em 10 e terminando em 0. Mostrar uma mensagem “FIM!” apos a
+//contagem.
+
 //Make a program with while and show a desceding order between 10 and 0
 #include <stdio.h>
 int main(){
