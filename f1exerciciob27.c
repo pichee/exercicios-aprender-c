a8d05d24ed37d45e79a0343e28c3e5bf1a168e54
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:23:15 -0300

    Update and rename exerciciob27.c to f1exerciciob27.c

--- f1exerciciob27.c
+++ f1exerciciob27.c
@@ -1,3 +1,6 @@
+//Leia um valor de area em hectares e apresente-o convertido em metros quadrados m2
+//A formula de conversao e:  M = H ∗ 10000, sendo M a area em metros quadrados e H
+//a area em hectares. 
 //Read a value from area in hectares and show convert in square meters
 //the recipe formula the  use is :M = H ∗ 10000, be M the area in square meter and
 //H a area in hectares.
