11366d16eecf11a34d54f179451ea8743d46ea7d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:39:21 -0300

    Update and rename exerciciok116.c to f3exercicioc24.c

--- f3exercicioc24.c
+++ f3exercicioc24.c
@@ -1,3 +1,7 @@
+//Escreva um programa que leia um numero inteiro e calcule a soma de todos os divisores 
+//desse numero, com excecao dele proprio. Ex: a soma dos divisores do numero 66  e
+//1 + 2 + 3 + 6 + 11 + 22 + 33 = 78
+
 //Addition your divisirors;
 #include <stdio.h>
 int main(){
