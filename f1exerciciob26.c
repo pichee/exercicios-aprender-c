e99f129c63ff291ee29da158c00b8f27e303b8aa
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:21:50 -0300

    Update and rename exerciciob26.c to f1exerciciob26.c

---
+++ f1exerciciob26.c
@@ -0,0 +1,12 @@
+//Leia um valor de area em metros quadrados m2 e apresente-o convertido em hectares.
+//A formula de conversao e: H = M ∗ 0, 0001, sendo M a area em metros quadrados e  H
+//a area em hectares.
+//Read a value in square meters and show in Hectares
+#include <stdio.h>
+int main(){
+    float m2,h;
+    printf("Put the  value in square meters ");
+    scanf("%f",&m2);
+    h= m2*0.0001;
+    printf("this value is equals  %.2f hectares",m2);
+}
