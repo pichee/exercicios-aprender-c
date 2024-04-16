4c4ba15d5ff698b2cd943f174fb1d634699f4da8
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 17:21:21 -0300

    Update and rename exerciciok111.c to f3exercicio18c.c

--- f3exercicio18c.c
+++ f3exercicio18c.c
@@ -1,3 +1,9 @@
+//Escreva um algoritmo que leia certa quantidade de numeros e imprima o maior deles e
+//quantas vezes o maior numero foi lido. A quantidade de numeros a serem lidos deve ser
+//fornecida pelo usuario.
+
+//Write an algorithm that reads a certain amount of numbers and prints the largest one, as well as how many times
+//the largest number was read. The amount of numbers to be read should be provided by the user.
 #include <stdio.h>
 int main(){
     int a,r,n;
