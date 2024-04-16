2fdf32c6974b918d9170ea63783123f08099114b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:32:43 -0300

    Update and rename exercicioc40.c to f1exercicioc40.c

--- f1exercicioc40.c
+++ f1exercicioc40.c
@@ -1,3 +1,6 @@
+//Uma empresa contrata um encanador a R$ 30,00 por dia. Faca um programa que solicite
+//o numero de dias trabalhados pelo encanador e imprima a quantia lıquida que devera ser
+//paga, sabendo-se que sao descontados 8% para imposto de renda.
 //a company hire a plumber  for  R$ 30,00 each day.Make a program what request
 //the number the eork days for plumberr and show amount  lıquid he receved´
 //know 8% is discont because the tax.
