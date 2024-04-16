0dd179232dbecc5eb69903c44987d2f25ee09f40
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 19:08:32 -0300

    Update f3exercicioc40.c

--- f3exercicioc40.c
+++ f3exercicioc40.c
@@ -1,5 +1,6 @@
 //Elabore um programa que faca leitura de varios numeros inteiros, ate que se digite um
 //numero negativo. O programa tem que retornar o maior e o menor numero lido.
+//Develop a program that reads several integer numbers until a negative number is entered. The program should return the largest and smallest numbers entered.
 #include <stdio.h>
 
 int main() {
