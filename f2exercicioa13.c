1d262892379dfe9783847384d3f5f4a709f3fc71
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:58:16 -0300

    Update and rename exerciciod66.c to f2exercicioa13.c

--- f2exercicioa13.c
+++ f2exercicioa13.c
@@ -1,3 +1,8 @@
+//Faca um algoritmo que calcule a media ponderada das notas de 3 provas. A primeira e
+//a segunda prova tem peso 1 e a terceira tem peso 2. Ao final, mostrar a media do aluno 
+//e indicar se o aluno foi aprovado ou reprovado. A nota para aprovacao deve ser igual ou 
+//superior a 60 pontos.
+
 //Show a person how is your media with 3 notes for spend in test the note is 6;
 #include <stdio.h>
 int main(){
