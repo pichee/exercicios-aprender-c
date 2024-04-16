17ddc4f965e59c357734f4ed561e0ea19fbcb492
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 14:34:11 -0300

    Update and rename exerciciog92.c to f2exercicioc39.c

--- f2exercicioc39.c
+++ f2exercicioc39.c
@@ -1,11 +1,20 @@
+//Uma empresa decide dar um aumento aos seus funcionarios de acordo com uma tabela 
+//que considera o salario atual e o tempo de servico de cada funcionario. Os funcionarios
+//com menor salario terao um aumento proporcionalmente maior do que os funcionarios
+//com um salario maior, e conforme o tempo de servico na empresa, cada funcionario ir a
+//receber um bonus adicional de salario. Faca um programa que leia:
+//• o valor do salario atual do funcionario;
+//• o tempo de servico desse funcionario na empresa (numero de anos de trabalho na
+//empresa).
+
 //verify the salary of the employee and how many yeears he has in the company,next
 //using the frame for increase your salarys
 //SALARY ´ READJUSTMENT(%) WORK TIME Bonus ˆ
-until 500,00 ´ 25% smaller to  1 year no bonus ˆ
-until 1000,00 ´ 20%  1 between 3 years 100,00
-until 1500,00 ´ 15%  4 between 6 years 200,00
-until 2000,00 ´ 10%  7 beetween 10 years 300,00
-above 2000,00 no readjustmente more than 10 years 500,00
+//until 500,00 ´ 25% smaller to  1 year no bonus ˆ
+//until 1000,00 ´ 20%  1 between 3 years 100,00
+//until 1500,00 ´ 15%  4 between 6 years 200,00
+//until 2000,00 ´ 10%  7 beetween 10 years 300,00
+//above 2000,00 no readjustmente more than 10 years 500,00
 
 #include <stdio.h>
 
