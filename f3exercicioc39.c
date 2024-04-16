8fef0e39c5b7bce1c55cbb98dce63e1f18ce0033
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:05:14 -0300

    Update and rename exerciciom131.c to f3exercicioc39.c

--- f3exercicioc39.c
+++ f3exercicioc39.c
@@ -1,3 +1,7 @@
+//Faca um programa que calcule a area de um triangulo, cuja base e altura sao fornecidas
+//pelo usuario. Esse programa nao pode permitir a entrada de dados invalidos, ou seja,
+//medidas menores ou iguais a 0.
+
 //calculate the other value the triangule,know the height and the base
 #include <stdio.h>
        int main(){
