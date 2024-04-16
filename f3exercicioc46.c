9e8367be3bb88a7528340b69bbe34a09c1255c0d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:51:24 -0300

    Update and rename exerciciom137.c to f3exercicioc46.c

--- f3exercicioc46.c
+++ f3exercicioc46.c
@@ -1,3 +1,9 @@
+//Fac¸a um programa que gera um numero aleatorio de 1 a 1000. O usuario deve tentar
+//acertar qual o numero foi gerado, a cada tentativa o programa dever a informar se o
+//chute e menor ou maior que o numero gerado. O programa acaba quando o usuario
+//acerta o numero gerado. O programa deve informar em quantas tentativas o numero foi 
+//descoberto.
+
 //Make a program geranete a number between 1 than 1000 and make the user accept
 //show if the number is bigger or smaller.
 
