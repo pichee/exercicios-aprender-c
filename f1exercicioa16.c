407d296b2f9a7d029123be500773d39b3f25b2ea
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 12:57:27 -0300

    Update and rename exercicioa16.c to f1exercicioa16.c

--- f1exercicioa16.c
+++ f1exercicioa16.c
@@ -1,11 +1,12 @@
 //Leia um valor de comprimento em polegadas e apresente-o convertido em centımetros.
 //A formula de conversao˜ e:´ C = P ∗ 2, 54, sendo C o comprimento em centımetros e P o
 //comprimento em polegadas.
+//Read a value in inches and show in centimeter
 #include <stdio.h>
 int main(){
     float p,c;
-    printf("digite seu valor em polegadas");
+    printf("Enter how much inches );
     scanf("%f",&p);
     c=p*2.54;
-    printf("Esse valor equivale a %.2f centımetros",c);
+    printf("This is igual %.2f centimeters",c);
 }
