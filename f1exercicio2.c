c7728ea2255afea4001a66718b32f646cdc498a9
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 09:34:59 -0300

    Update and rename exercicio2.c to f1exercicio2.c

---
+++ f1exercicio2.c
@@ -0,0 +1,10 @@
+//Faca um programa que leia um numero real e o imprima.
+//Read a float number and show.
+#include <stdio.h>
+int main()
+{
+    float n;
+    printf("Enter a number:");
+    scanf("%f",&n);
+    printf("%.2f",n);
+}
