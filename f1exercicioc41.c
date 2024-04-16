d8572875802a039c21c2725bbd495290851b22ce
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:33:31 -0300

    Update and rename exercicioc41.c to f1exercicioc41.c

--- f1exercicioc41.c
+++ f1exercicioc41.c
@@ -1,3 +1,7 @@
+//Faca um programa que leia o valor da hora de trabalho (em reais) e numero de horas
+//trabalhadas no mes. Imprima o valor a ser pago ao funcionario, adicionando 10% sobre
+//o valor calculado.
+
 //Make a program in read the value in hours work(in Reais) and de number worked hours in the mouth.
 //Show the value being paid out the employee,add 10% about the value calculate.
 #include <stdio.h>
