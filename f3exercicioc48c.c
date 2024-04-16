f9197a6f2a0fd2d244f66db7cbfd1a1ee588a23f
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:48:34 -0300

    Update and rename f3exercicio48c.c to f3exercicioc48c.c

--- f3exercicioc48c.c
+++ f3exercicioc48c.c
@@ -1,6 +1,7 @@
-// show the fibonacci sequence until 4000000
-//Fac¸a um programa que some os termos de valor par da sequencia de Fibonacci, cujos
+//Faca um programa que some os termos de valor par da sequencia de Fibonacci, cujos
 //valores nao ultrapassem quatro milhoes.
+
+// show the fibonacci sequence until 4000000
 #include <stdio.h>
 int main(){
     int a=0,n,n1=1,g,c=1;
