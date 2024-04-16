7825fab125bf5e09618b74910bc30dbf188bb132
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:14:54 -0300

    Update and rename exerciciol126.c to f3exercicioc34.c

--- f3exercicioc34.c
+++ f3exercicioc34.c
@@ -1,3 +1,7 @@
+//Faca um programa que calcule o menor numero divisıvel por cada um dos numeros de 1
+//a 20? Ex: 2520 e o menor numero que pode ser dividido por cada um dos numeros de 1
+//a 10, sem sobrar resto.
+
 //Make a program show the multiple the a number in 1 between a 20.
 #include <stdio.h>
 int main(){
