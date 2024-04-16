e8d2a7566e0f2a3615b6589858e9876d2634a9e8
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:44:17 -0300

    Update and rename exercicion143.c to f3exerciciod52.c

--- f3exerciciod52.c
+++ f3exerciciod52.c
@@ -1,3 +1,8 @@
+//Escreva um programa que receba como entrada o valor do saque realizado pelo cliente
+//de um banco e retorne quantas notas de cada valor serao necessarias para atender ao
+//saque com a menor quantidade de notas poss´ıvel. Serao utilizadas notas de 100, 50,
+//20, 10, 5, 2 e 1 real.
+
 //Write a program that receives as input the value of the withdrawal made by the customer
 //from a bank and returns how many notes of each denomination will be required to fulfill the
 //withdrawal with the smallest possible number of notes. Notes of 100, 50,
