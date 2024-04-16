b6d4d1de7c754a9dbf03a3448a1b942d597352ad
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:57:27 -0300

    Update and rename exerciciod65.c to f2exercicioa12.c

--- f2exercicioa12.c
+++ f2exercicioa12.c
@@ -1,3 +1,6 @@
+//Ler um numero inteiro. Se o numero lido for negativo, escreva a mensagem “Numero
+//invalido”. Se o numero for positivo, calcular o logaritmo deste numero. 
+
 //Show a number and if is positive show the log if is negative show invalid number
 #include <stdio.h>
 #include <math.h>
