8bf203e4a36726237f0c1dce1922380e103ce3ae
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 12:55:20 -0300

    Update and rename exercicioa14.c to f1exercicioa14.c

--- f1exercicioa14.c
+++ f1exercicioa14.c
@@ -1,11 +1,12 @@
 //Leia um angulo em graus e apresente-o convertido em radianos. A formula de conversao
 //e:R = G ∗ π/180, sendo G o angulo em graus e R em radianos e π = 3.14.
+//Read  a angle in degrees and show in radians;
 #include <stdio.h>
 int main(){
     float r,g;
     float pi=3.14;
-    printf("O seu angulo possui quantos graus");
+    printf("How much is your angle");
     scanf("%f",&g);
     r=g *pi/180;
-    printf("A conversao em radianos é igual a %.2f",r);
+    printf("This is igual %.2f degrees",r);
 }
