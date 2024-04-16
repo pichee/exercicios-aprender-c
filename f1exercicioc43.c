a78efe51d17d9f21db979883363a85c681fa5915
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:34:55 -0300

    Update and rename exercicioc43.c to f1exercicioc43.c

--- f1exercicioc43.c
+++ f1exercicioc43.c
@@ -1,3 +1,9 @@
+//Escreva um programa de ajuda para vendedores. A partir de um valor total lido, mostre:
+//• o total a pagar com desconto de 10%;
+//• o valor de cada parcela, no parcelamento de 3× sem juros;
+//• a comissao do vendedor, no caso da venda ser a vista (5% sobre o valor com desconto)
+//• a comissao do vendedor, no caso da venda ser parcelada (5% sobre o valor total)
+
 //write a program for help sellers.Show:
 //•The total to pay with 10% discont;
 //•The value of each portion,in portion of 3x without fees;
