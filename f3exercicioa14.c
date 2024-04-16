27c4a1b932d0391b3eb6d8b78644104ff324561d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 17:26:32 -0300

    Update f3exercicioa14.c

--- f3exercicioa14.c
+++ f3exercicioa14.c
@@ -1,17 +1,22 @@
-//Faca um programa que leia um numero inteiro positivo par N e imprima todos os numeros
+//Faca um programa que leia um numero inteiro positivo par  N e imprima todos os numeros
 //pares de 0 ate N em ordem decrescente.
-//Make a program show descending order of the a number;
+//Make a program read the a number and show the number pairs  until  in order decrescent;
+
+
 #include <stdio.h>
 int main(){
     int c;
-    int n,a;
+    int n,a,a1;
 
-    printf("Enter a  whole number\n");
+    printf("Enter a  whole number par\n");
     scanf("%i",&n);
-    c=n;
+    c=n%2;
+    a1=n;
+    if(c==0){
     while(a<=n){
-        printf("\n%i",c);
-        c=c-1;
+        
+        printf("\n%i",a1);
+        a1=a1-2;
         a++;
-    }
-}
+        a++;
+    }}}
