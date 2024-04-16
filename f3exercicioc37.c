cc646ae4b78a6d2ce3ef3dbd0a89b0c65b8543e7
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 16:03:46 -0300

    Update and rename exerciciol129.c to f3exercicioc37.c

--- f3exercicioc37.c
+++ f3exercicioc37.c
@@ -1,3 +1,10 @@
+//Escreve um programa que verifique quais numeros entre 1000 e 9999 (inclusive) pos
+//suem a propriedade seguinte: a soma dos dois dıgitos de mais baixa ordem com os dois
+//dıgitos de mais alta ordem elevada ao quadrado e igual ao proprio numero. Por exemplo,
+//para o inteiro 3025, temos que:
+//30 + 25 = 55
+//552 = 3025
+
 //Enter a number between 1000 and 9999 addition ben 2 and make the squarle
 #include <stdio.h>
 int main(){
