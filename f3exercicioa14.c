9e5145f71037c7782542e93361fc4ae71280f03c
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 17:15:57 -0300

    Update and rename exercicioj106.c to f3exercicioa14.c

--- f3exercicioa14.c
+++ f3exercicioa14.c
@@ -1,3 +1,5 @@
+//Faca um programa que leia um numero inteiro positivo par N e imprima todos os numeros
+//pares de 0 ate N em ordem decrescente.
 //Make a program show descending order of the a number;
 #include <stdio.h>
 int main(){
