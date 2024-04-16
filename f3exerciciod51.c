274b2fab8adadbf3eda34fe50287bce888e6f9f5
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:45:11 -0300

    Update and rename exercicion142.c to f3exerciciod51.c

--- f3exerciciod51.c
+++ f3exerciciod51.c
@@ -1,3 +1,7 @@
+//Um funcionario recebe aumento anual. Em 1995 foi contratado por 2000 reais. Em 1996
+//recebeu aumento de 1.5%. A partir de 1997, os aumentos sempre correspondem ao
+//dobro do ano anterior. Faca programa que determine o salario atual do funcionario. 
+
 //An employee receives annual raises. In 1995, he was hired for 2000 reais.
 //In 1996, he received a 1.5% raise. Starting in 1997, raises always correspond to double the previous year's.
 //Create a program to determine the current salary of the employee.
