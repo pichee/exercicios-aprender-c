2c38952b05f03cf8adeaaa11085cb09821351feb
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 09:37:27 -0300

    Update and rename exercicio3.c to f1exercicio3.c

--- f1exercicio3.c
+++ f1exercicio3.c
@@ -1,4 +1,5 @@
 //Peca ao usuario para digitar tres valores inteiros e imprima a soma deles.
+//Ask the user enter three numbers and show the sum.
 #include <stdio.h>
 
 int
@@ -7,11 +8,11 @@ main ()
     int a=0;
     int n,s;
      while (a<3){
-         printf("digite um número inteiro");
+         printf("Enter a number:");
          scanf("%i",&s);
          n=n+s;
          a++;
                 }
-    printf("a soma dos tres valores é igual a %i",n);
+    printf("the sum is igual %i",n);
 
 }
