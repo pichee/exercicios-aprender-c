653642c02c22fd9f9c506549a4bf893ca89176a5
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:54:34 -0300

    Update and rename exerciciod61.c to f2exercicio8.c

--- f2exercicio8.c
+++ f2exercicio8.c
@@ -1,3 +1,9 @@
+//Faca um programa que leia 2 notas de um aluno, verifique se as notas sao validas e
+//exiba na tela a media destas notas. Uma nota valida deve ser, obrigatoriamente, um 
+//valor entre 0.0 e 10.0, onde caso a nota nao possua um valor valido, este fato deve ser
+//informado ao usuario e o programa termina.
+
+
 //a program what read two notes to evaluation and show the final note,if the note is above ten
 //show is Something is incorret
 #include <stdio.h>
