8cde586d9c15c4020c29f88d3ebab2dee4b23f57
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:34:13 -0300

    Update and rename exercicioj105.c to f3exercicioa12.c

--- f3exercicioa12.c
+++ f3exercicioa12.c
@@ -1,3 +1,7 @@
+//Faca um programa que leia um numero inteiro positivo N e imprima todos os numeros
+//naturais de 0 ate N em ordem decrescente.
+
+
 //Make a program show de ascending order of the a number;
 #include <stdio.h>
 int main(){
