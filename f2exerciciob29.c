e7db708b6984680bc07868c4e09add44a586008c
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 14:23:48 -0300

    Update and rename exerciciof82.c to f2exerciciob29.c

--- f2exerciciob29.c
+++ f2exerciciob29.c
@@ -1,3 +1,9 @@
+//Fac¸a uma prova de matematica para crianc¸as que estao aprendendo a somar numeros
+//inteiros menores do que 100. Escolha numeros aleatorios entre 1 e 100, e mostre na
+//tela a pergunta: qual e a soma de  a + b, onde a e b sao os numeros aleatorios. Peca a
+//resposta. Faca cinco perguntas ao aluno, e mostre para ele as perguntas e as respostas
+//corretas, alem de quantas vezes o aluno acertou.
+
 //Make a math test for children who are learning to add integers less than 100.
 //Choose random numbers between 1 and 100, and display the question on the screen:
 //"What is the sum of a + b", where a and b are the random numbers. Ask for the answer.
