608eb2bcc04af4d90828eca6b8797bc18be2fae2
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 09:47:36 -0300

    Update and rename exercicio8.c to f1exercicio8.c

--- f1exercicio8.c
+++ f1exercicio8.c
@@ -1,11 +1,12 @@
 //Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius. A
 //formula de convers ´ ao˜ e:´ C = K − 273.15, sendo C a temperatura em Celsius e K a
 //temperatura em Kelvin.
+//Read a temperature in Kelvin and show in Celsius:
 #include <stdio.h>
 int main(){
     float k,c;
-    printf("digite a temperaturaem graus Kelvin ");
+    printf("Enter the temperature in Kelvin: ");
     scanf("%f",&k);
     c= k-273.15;
-    printf("essa temperatura em Celsius equivale a %.2f",c);
+    printf("This temperature is igual %.2f Celsius",c);
 }
