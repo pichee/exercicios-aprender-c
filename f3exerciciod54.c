4e3627942d2510143bacfa0e0ce3f32d998623e8
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:42:32 -0300

    Update and rename exercicion145.c to f3exerciciod54.c

--- f3exerciciod54.c
+++ f3exerciciod54.c
@@ -1,4 +1,6 @@
-//verify if is a number odd or not
+//Fac¸a um programa que receba um numero inteiro maior do que 1, e verifique se o numero 
+//fornecido e primo ou nao.
+//verify if is a number prime or not
 #include <stdio.h>
 
 int main() {
@@ -6,7 +8,7 @@ int main() {
     printf("Enter a number bigger than 1\n");
     scanf("%i",&n);
     if(n>1 && n%2==1){
-        printf("This number is odd");
+        printf("This number is prime");
     }
     else{
         printf("ERROR");
