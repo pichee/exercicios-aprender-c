ae0112562446bc158ffae26881e0235058819e16
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:16:28 -0300

    Update and rename exerciciob22.c to f1exerciciob22.c

--- f1exerciciob22.c
+++ f1exerciciob22.c
@@ -1,11 +1,12 @@
 //Leia um valor de comprimento em jardas e apresente-o convertido em metros. A formula ´
 //de conversao˜ e: ´ M = 0, 91 ∗ J, sendo J o comprimento em jardas e M o comprimento
 //em metros
+//Read a value in yards and show in meters
 #include <stdio.h>
     int main(){
         float m,j;
-        printf("qual o comprimento em jardas ");
+        printf("How much yards:");
         scanf("%f",&j);
         m=0.91*j;
-        printf("essa quantida de jardas equivale a %.2f metros",m);
+        printf("This is igual %.2f meters",m);
     }
