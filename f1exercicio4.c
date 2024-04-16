a01a296c10d9bb54fa2483103e377cc9547d2296
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 09:39:30 -0300

    Update and rename exercicio4.c to f1exercicio4.c

--- f1exercicio4.c
+++ f1exercicio4.c
@@ -1,13 +1,14 @@
 //Leia um numero real e imprima o resultado do quadrado desse numero.
+//Read a float number and show the result in the square.
 #include <stdio.h>
 
 int
 main ()
 {
     float n;
-  printf ("Digite um número");
+  printf ("Enter a number:");
   scanf("%f",&n);
   n=n*n;
-  printf("O quadrado desse número é %.2f",n);    
+  printf("The square is %.2f",n);    
   return 0;
 }
