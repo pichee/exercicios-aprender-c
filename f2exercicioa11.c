70e58237ce4c5caa12bdeabd8ca3a1774b90c841
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 13:56:51 -0300

    Update and rename exerciciod64.c to f2exercicioa11.c

--- f2exercicioa11.c
+++ f2exercicioa11.c
@@ -1,3 +1,8 @@
+//Escreva um programa que leia um numero inteiro maior do que zero e devolva, na tela, a
+//soma de todos os seus algarismos. Por exemplo, ao numero 251 correspondera o valor 
+//8 (2 + 5 + 1). Se o numero lido nao for maior do que zero, o programa terminar a com a
+//mensagem “Numero inv ´ alido”.
+
 //Make a program that soma the numbers exemplo
 //987=(9+8+7),if the number is zero show invalid number
 #include <stdio.h>
