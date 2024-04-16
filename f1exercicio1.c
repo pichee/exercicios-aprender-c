6fde277aa604b787cf37409a76a016f661819da0
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 09:33:24 -0300

    Update and rename exercicio1.c to f1exercicio1.c

--- f1exercicio1.c
+++ f1exercicio1.c
@@ -1,11 +1,10 @@
 //Faca um programa que leia um numero inteiro e o imprima.
+//Make a program show a whole number.
 #include <stdio.h>
 int main()
 {
     int n;
-    printf("Digite um número inteiro");
+    printf("Enter a number:");
     scanf("%i",&n);
     printf("%i",n);
-
-    return(0);
 }
