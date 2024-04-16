bdcbe1b53fa0869bfad49ffd23ea760f143c80a3
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:27:03 -0300

    Update and rename exerciciog98.c to f3exercicio4.c

--- f3exercicio4.c
+++ f3exercicio4.c
@@ -1,3 +1,5 @@
+//Escreva um programa que declare um inteiro, inicialize-o com 0, e incremente-o de 1000
+//em 1000, imprimindo seu valor na tela, ate que seu valor seja 100000 (cem mil)
 //make a program star in zero and end in one hundred thousand
 #include <stdio.h>
 int main(){
