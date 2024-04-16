5634567054b1436d739f686cc246822184f2e624
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-16 15:23:35 -0300

    Update and rename exerciciog95.c to f3exercicio1.c

--- f3exercicio1.c
+++ f3exercicio1.c
@@ -1,3 +1,5 @@
+//Faca um programa que determine o mostre os cinco primeiros multiplos de 3, conside
+//rando numeros maiores que 0.
 //Show the five firsts multiple the three
 #include <stdio.h>
     int main(){
