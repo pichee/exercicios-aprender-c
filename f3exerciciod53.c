2bc367f476d1025c7418b784b2f7c108cfc12702
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-18 13:27:50 -0300

    Update f3exerciciod53.c

--- f3exerciciod53.c
+++ f3exerciciod53.c
@@ -1,4 +1,4 @@
-//Escreva um programa que leia um numero inteiro positivo ´ n e em seguida imprima n
+//Escreva um programa que leia um numero inteiro positivo n e em seguida imprima n
 // linhas do chamado Triangulo de Floyd.
 //Write a program that reads a positive integer 'n' and then prints n lines of the so-called Floyd's Triangle. For n = 6, we have:
 #include <stdio.h>
@@ -6,21 +6,20 @@
 int main() {
     int altura;
 
-    // Solicita ao usuário a altura do triângulo
     printf("Digite a altura do Triangulo de Floyd: ");
     scanf("%d", &altura);
 
-    // Variável para controlar o valor a ser impresso no triângulo
+
     int valor = 1;
 
-    // Loop para cada linha do triângulo
+    
     for (int linha = 1; linha <= altura; linha++) {
-        // Loop para imprimir os valores da linha atual
+     
         for (int coluna = 1; coluna <= linha; coluna++) {
             printf("%d ", valor);
-            valor++; // Incrementa o valor para o próximo número
+          
         }
-        printf("\n"); // Pula para a próxima linha após imprimir todos os valores
+        printf("\n");
     }
 
     return 0;
