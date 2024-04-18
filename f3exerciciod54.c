08fe34556efbcc34c8ac0086e3da03d008ac7992
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-18 11:59:56 -0300

    Update f3exerciciod54.c

--- f3exerciciod54.c
+++ f3exerciciod54.c
@@ -1,19 +1,26 @@
-//Fac¸a um programa que receba um numero inteiro maior do que 1, e verifique se o numero 
+//Faca um programa que receba um numero inteiro maior do que 1, e verifique se o numero 
 //fornecido e primo ou nao.
 //verify if is a number prime or not
 #include <stdio.h>
 
 int main() {
-    int n;
-    printf("Enter a number bigger than 1\n");
-    scanf("%i",&n);
-    if(n>1 && n%2==1){
-        printf("This number is prime");
+    int n, j, p, c = 0;
+
+    printf("Enter a number bigger than 1: ");
+    scanf("%i", &n);
+
+    for (p = 2; p <= n / 2; p++) {
+        if (n % p == 0) {
+            c = 1;
+            break;
+        }
     }
-    else{
-        printf("ERROR");
+
+    if (c == 0 && n != 1) {
+        printf("%i is a prime number.\n", n);
+    } else {
+        printf("%i is not a prime number.\n", n);
     }
-    
-    
+
     return 0;
 }
