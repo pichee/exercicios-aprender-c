f71d37018299f152c5a7c1da4e2af908e72dd243
Gustavo <76635390+pichee@users.noreply.github.com>
2024-04-23 14:49:48 -0300

    Create f4exercicioa15v.c

---
+++ f4exercicioa15v.c
@@ -0,0 +1,28 @@
+//Leia um vetor com 20 numeros inteiros. Escreva os elementos do vetor eliminando ele
+//mentos repetidos
+
+#include <stdio.h>
+
+int main() {
+    int v[20], vv[20], a, c, s = 0;
+    for (a = 0; a < 20; a++) {
+        printf("Enter a number:\n");
+        scanf("%i", &v[a]);
+    }
+    for (a = 0; a < 20; a++) {
+        int duplicado = 0;
+        for (c = 0; c < s; c++) {
+            if (v[a] == vv[c]) {
+                duplicado = 1;
+                break;
+            }
+        }
+        if (!duplicado) {
+            vv[s] = v[a];
+            s++;
+        }
+    }
+    for (a = 0; a < s; a++) {
+        printf("%i ", vv[a]);
+    }
+}
