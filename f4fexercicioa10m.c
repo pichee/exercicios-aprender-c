25d8ab7049ebc5153dd0a981bedd8061aaa7b0e2
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-02 16:07:43 -0300

    Create f4fexercicioa10m.c

---
+++ f4fexercicioa10m.c
@@ -0,0 +1,24 @@
+//Leia uma matriz de 3 x 3 elementos. Calcule a soma dos elementos que estao na diago
+//nal principal
+
+//Read a matrix 3x3 and calculate the elements  principal diagonal
+#include <stdio.h>
+int main(){
+    int v[3][3];
+    int j,i,s;
+        for ( i = 0; i < 3; i++)
+        {
+            for (j= 0; j<3; j++)
+            {      
+                printf("Enter a number:");
+                scanf("%i",&v[i][j]);
+                if(i == 0 && j==0){
+                s+=v[i][j];}
+                if(i == 1 && j==1){
+                s+=v[i][j];
+                    }
+                if(i == 2 && j==2){
+                s+=v[i][j];}
+                }
+        }
+printf("The add the elements principal diagonal is %i",s);}
