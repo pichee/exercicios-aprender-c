7e07a96d0c5d1cd046934d0fca5c29abc4300ce4
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-28 15:09:01 -0300

    Create f7exercicioa51.c

---
+++ f7exercicioa51.c
@@ -0,0 +1,25 @@
+//Faca uma funcao que receba uma matriz de 3 x 3 elementos. Calcule e retorne a soma
+//dos elementos que estao na diagonal secundaria.
+//Make a function receb a matrix 3x3 and calculate the elements that stayin in the secondary diagonal
+#include <stdio.h>
+    int matriz(float aux[3][3]){
+        int a=0,b=0;
+        float soma;
+        for (a = 0; a < 3; a++)
+        {
+            b=0;
+            for (b = 0; b < 3; b++)
+            {
+                printf("Enter a number for vector [%i][%i]:",a+1,b+1);
+                scanf("%f",&aux[a][b]);
+            }
+        
+        }
+        soma=aux[0][2]+aux[1][1]+aux[0][1];
+        printf("There addition in secondary diagonal is %.2f",soma);
+        
+    }
+    int main(){
+        float v[3][3];
+        matriz(v);
+    }
