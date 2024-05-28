898f31c8e58b3370c826ea1f82749a6c5ea27f3b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-28 14:56:19 -0300

    Create f7exercicioa49.c

---
+++ f7exercicioa49.c
@@ -0,0 +1,26 @@
+//Faca uma funcao que receba uma matriz de 3 x 3 elementos. Calcule a soma dos
+//elementos que estao abaixo da diagonal principal. 
+
+//Make a function receb a matrix 3x3 and calculate the elements that stay down the principal diagonal
+#include <stdio.h>
+    int matriz(float aux[3][3]){
+        int a=0,b=0;
+        float soma;
+        for (a = 0; a < 3; a++)
+        {
+            b=0;
+            for (b = 0; b < 3; b++)
+            {
+                printf("Enter a number for vector [%i][%i]:",a+1,b+1);
+                scanf("%f",&aux[a][b]);
+            }
+        
+        }
+        soma=aux[1][0]+aux[3][0]+aux[3][1];
+        printf("There addition numbers down principal diagonal is %.2f",soma);
+        
+    }
+    int main(){
+        float v[3][3];
+        matriz(v);
+    }
