e15084985930d9d4584fc9b8115f889d16b46cff
Gustavo <76635390+pichee@users.noreply.github.com>
2024-05-30 11:18:33 -0300

    Create f7exercicioa65.c

---
+++ f7exercicioa65.c
@@ -0,0 +1,63 @@
+//Implemente a func¸ao a qual recebe duas strings, str1 e str2, e um valor inteiro positivo
+//N. A funcao concatena nao mais que  N caracteres da string apontada por str2 a string 
+//apontada por str1 e termina str1 com NULL.
+
+//I don't undestamd the difference the exercise 64 for 65
+
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+int string(char *str1,char *str2){
+    int i=0,a=0,l=0,l1=0;
+    i=strlen(str1);
+    a=strlen(str2);
+    int aux=a;
+    while (getchar() != '\n');
+    while (a==aux)
+    {
+        printf("How many letters do you want the string 1:");
+        scanf("%i",&l);
+        if(l>i){
+            printf("Please select a number valid until %i:\n",i);
+           while (getchar() != '\n');
+        }else{
+            break;
+        }
+    }
+    getchar();
+    while (a==aux)
+    {
+        printf("How many letters do you want the string 2:");
+        scanf("%i",&l1);
+        if(l1>a){
+            printf("Please select a number valid until %i:\n",a);
+            while (getchar() != '\n');
+        }else{
+            break;
+        }
+    }
+    int t=0;
+    while (t<l)
+    {
+    printf("%c",str1[t]);
+    t++;
+    }
+    t=0;
+    while (t<l1)
+    {
+    printf("%c",str2[t]);
+    t++;
+    }
+    t=0;
+    
+    
+}
+    int main(){
+        char n[100],n1[100];
+        printf("Enter a string:");
+        scanf("%99[^\n]",n);
+        getchar();
+        printf("Enter other string:");
+        scanf("%99[^\n]",n1);
+        string(n,n1);
+    }
