84aad80681bb662a5e9170cb7663a5d37f121091
Gustavo <76635390+pichee@users.noreply.github.com>
2024-06-04 10:19:11 -0300

    Create f8exercicioa22.c

---
+++ f8exercicioa22.c
@@ -0,0 +1,6 @@
+//Uma sequencia de Fibonacci generalizada, de f0 a f1 ˆ e definida como fibg(f0, f1, 0), ´
+//fibg(f0, f1, 1), fibg(f0, f1, 2), ..., onde:
+//fibg(f0, f1, 0) = f0
+//fibg(f0, f1, 1) = f1
+//fibg(f0, f1, n) = fibg(f0, f1, n-1) + fibg(f0, f1, n-2), se n > 1.
+//(Nao achei a pergunta)
