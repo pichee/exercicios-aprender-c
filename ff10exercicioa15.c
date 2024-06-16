0e4fea9eb7178b99e892cf885f77eadb0747aed9
Gustavo <76635390+pichee@users.noreply.github.com>
2024-06-15 22:00:18 -0300

    Update ff10exercicioa15.c

--- ff10exercicioa15.c
+++ ff10exercicioa15.c
@@ -1,3 +1,9 @@
+//Faca um programa que leia dois numeros N e M e:
+//• Crie e leia uma matriz de inteiros N x M;
+//• Localize os tres maiores numeros de uma matriz e mostre a linha e a coluna onde
+//estao.
+
+//Show the tree bigger numbers in a matrix
 #include <stdio.h>
 #include <stdlib.h>
 
