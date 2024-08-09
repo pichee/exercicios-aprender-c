c022643360fde729cecdea2732504b33c0bf6950
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-09 11:53:23 -0300

    Atualizar o exercicio003.c

--- exercicio001/exercicio003.c
+++ exercicio001/exercicio003.c
@@ -1,18 +1,17 @@
 //Peca ao usuario para digitar tres valores inteiros e imprima a soma deles.
 //Ask the user enter three numbers and show the sum.
 #include <stdio.h>
-
-int
-main ()
+int somanumeros(int a,int b,int c){
+return a+b+c;
+}
+int main ()
 {
-    int a=0;
-    int n,s;
-     while (a<3){
-         printf("Enter a number:");
-         scanf("%i",&s);
-         n=n+s;
-         a++;
-                }
-    printf("the sum is igual %i",n);
+    int numeros[3],soma=0,a=0;
+    for(a=0;a<3;a++){
+    printf("Enter the %i°  while number:",a+1);
+    scanf("%i",&numeros[a]);
+    }
+    soma=somanumeros(numeros[1],numeros[2],numeros[0]);
+    printf("The add This numbers is %i",soma);
 
 }
