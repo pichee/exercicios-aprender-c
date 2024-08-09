29dddbe3361bda8ca5438f4699b188f1f6581fbc
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-09 19:31:06 -0300

    Update exercicio009.c

--- exercicio001/exercicio009.c
+++ exercicio001/exercicio009.c
@@ -3,10 +3,15 @@
 //temperatura em Kelvin.
 //Read a temperature in Celsius and show in kelvin:
 #include <stdio.h>
+int conversao_temperaturas(float a){
+    float result=0;
+    result=a+273.15;
+    return result;
+}
 int main(){
-    float c,k;
-    printf("Enter a temperature in Celsius");
-    scanf("%f",&c);
-    k= c+273.15;
-    printf("this temperature is igual %.2f Kelvin",k);
+    float celsius,resultado;
+    printf("Enter a temperature in Celsius:");
+    scanf("%f",&celsius);
+    resultado=conversao_temperaturas(celsius);
+    printf("this temperature is igual %.2f Kelvin",resultado);
 }
