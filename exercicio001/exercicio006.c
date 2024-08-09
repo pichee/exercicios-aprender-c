e945bbb6d8a574f8c72333d3416b9f1903c32acc
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-09 14:44:23 -0300

    Atualizar o exercicio006.c

--- exercicio001/exercicio006.c
+++ exercicio001/exercicio006.c
@@ -4,14 +4,18 @@
 //Read a temperature in Celsius and show in Fahrenheit
 
 #include <stdio.h>
-
+int convertordetemperaturas(float celsius){
+    float fanhereit=0;
+ fanhereit=celsius*(9/5)+32;
+ return fanhereit;
+ }
 int main()
 {
-    float f,c;
-    printf("Enter the temperature in Celsius ");
-    scanf("%f",&c);
-    f=c*(9.0/5.0)+32.0;
-    printf("This temperature is igual %.2f Fahrenheit",f);
+    float result,temperatura;
+    printf("Enter the temperature in Celsius:");
+    scanf("%f",&temperatura);
+    result=convertordetemperaturas(temperatura);
+    printf("This temperature is igual %.2f Fahrenheit",result);
 
     return 0;
 }
