441cb2b24f30bc73a4f9c98a9f5d358043b366fd
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-14 19:37:16 -0300

    Update exercicio1 (2).c

--- exercicio004/exercicio1 (2).c
+++ exercicio004/exercicio1 (2).c
@@ -3,18 +3,35 @@
 //Read a matrix 4x4 and show how many values there is bigger than 10.
 #include <stdio.h>
     int main(){
-        int v[4][4];
-        int j,i,c;
-        for(j=0;j<4;j++){
-            for ( i = 0;i<4;i++)
+        int matrix[4][4];
+        int linha,coluna,contador_maior_que_10,pular=0;
+        for(linha=0;linha<4;linha++){
+            for (coluna = 0;coluna<4;coluna++)
             {
                 printf("Enter a number:");
-                scanf("%i",&v[j][i]);
-                if(v[j][i]>10){
-                    c++;
+                scanf("%i",&matrix[linha][coluna]);
+                if(matrix[linha][coluna]>10){
+                    contador_maior_que_10++;
                 }
             }
             
         }
-        printf("This matrix have %i values biggert than 10",c);
+        printf("----Matrix----\n");
+        for (linha= 0;linha < 4; linha++)
+        {
+            for (coluna= 0; coluna < 4; coluna++)
+            {
+                printf("%i ",matrix[linha][coluna]);
+                if (pular==3){
+                    printf("\n");
+                    pular=0;
+                }
+                else{
+                    pular++;
+                }
+            }
+            
+        }
+        
+        printf("-----------------\n %i values bigger than 10 in the matrix\n-----------------",contador_maior_que_10);
     }
