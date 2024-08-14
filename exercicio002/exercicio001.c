0da7e86b42fb03bb2c897278ef3e98f39a167f56
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-14 11:43:02 -0300

    Atualizar o exercicio001.c

--- exercicio002/exercicio001.c
+++ exercicio002/exercicio001.c
@@ -1,15 +1,18 @@
 //Make a program to receb two values in show the bigger.
 //Faca um programa que receba dois numeros e mostre qual deles e o maior
 #include <stdio.h>
-    int main(){
-        float n,n1;
-        printf("Put a number");
-        scanf("%f",&n);
-        printf("Put other number");
-        scanf("%f",&n1);
-        if (n>n1){
-            printf("The number %.2f is bigger than %.2f",n,n1);
+int maior(float numero1, float numero2){
+   if (numero1>numero2){
+            return printf("The number %.2f is bigger than %.2f",numero1,numero2);
         }else{
-            printf("The number %.2f is bigger than %.2f",n1,n);
+            return printf("The number %.2f is bigger than %.2f",numero2,numero1);
+        }}
+
+    int main(){
+        float numero1,numero2;
+        printf("Enter  a number:");
+        scanf("%f",&numero1);
+        printf("Enter other number:");
+        scanf("%f",&numero2);
+        maior(numero1,numero2);
         }
-    }
