c11e55d0ca9179e03df09e55822544bb95098a5c
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-15 07:37:19 -0300

    Atualizar o exerciciom003.c

--- exercicio004/exercicio3 (2).c
+++ exercicio004/exercicio3 (2).c
@@ -1,7 +1,7 @@
 //Faca um programa que preenche uma matriz 4 x 4 com o produto do valor da linha e da
 //coluna de cada elemento. Em seguida,imprima na tela a matriz.
 
-//make a matrix 4 x 4 and show the porduct the line by the colune.
+//make a matrix 4 x 4 and show the porduct the line by the colune
 
 
 #include <stdio.h>
