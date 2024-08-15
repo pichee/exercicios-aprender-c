9df6d818ad91272e8a09b60da87f01a1f582d44b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-15 16:28:46 -0300

    Atualizar o exercicio4 (2).c

--- exercicio004/exercicio4 (2).c
+++ exercicio004/exercicio4 (2).c
@@ -4,34 +4,39 @@
 //Read a matrix 4x4 and show the bigger value
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
     int main(){
-        int v[4][4];
-        int i,c,m,ac,al,ia;
-        printf("Enter a value:");
-        scanf("%i",&v[0][0]);
-        m=v[0][0];
-        ac=0;
-        al=0;
-        ia=0;
-        for(i=0;i<4;i++){
-            c=0;
-            ia=i;
-            for ( c = 0; c < 4; c++)
+        int matrix[4][4];
+        srand(time(NULL));
+        int maior=matrix[0][0];
+        int coluna_maior_numero=0;
+        int linha_maior_numero =0;
+        int pula_pula=0;
+        printf("-----------Matrix----------\n");
+        for(int coluna=0;coluna<4;coluna++){
+        
+            for (int linha= 0; linha < 4; linha++)
             {
-                printf("Enter a value:");
-                if(ia==0){
-                    c=1;
-                    ia++;
+                matrix[coluna][linha]=rand()%1000;
+                if(maior<matrix[coluna][linha]){
+                    maior=matrix[coluna][linha];
+                    linha_maior_numero=linha;
+                    coluna_maior_numero=coluna;
                 }
-                scanf("%i",&v[i][c]);
-                if(v[i][c]>m){
-                    m=v[i][c];
-                    ac=c;
-                    al=i;
+                if (pula_pula>3){
+                printf("\n");
+                pula_pula=1;
                 }
+                else{
+                pula_pula++;
+                }
+                printf("%i ",matrix[coluna][linha]);
 
             }
-            
+
         }
-        printf("The bigger value is %i in line %i and in %i colune",m,al,ac);
+        printf("\n---------------------------");
+        
+        printf("\nThe bigger value is %i in line %i and in %i colune",maior,linha_maior_numero+1,coluna_maior_numero+1);
     }
