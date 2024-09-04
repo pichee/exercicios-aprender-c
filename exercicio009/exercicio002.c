9b5a97ef47c2efe75d55557a3296f5ff820bf6d9
Gustavo <76635390+pichee@users.noreply.github.com>
2024-09-04 15:06:42 -0300

    Atualizar o exercicio002.c

--- exercicio009/exercicio002.c
+++ exercicio009/exercicio002.c
@@ -1,16 +1,20 @@
 //Escreva um programa que contenha duas variaveis inteiras. Compare seus enderecos e
 //exiba o maior endereco.
-
 //Read two variays that cont two int.compare your adress and the bigger adres:
 #include <stdio.h>
 int main(){
     int n1,n2;
     int *ponteiro1=&n1;
     int *ponteiro2=&n2;
+    printf("Digite o valor do primeiro numero:");
+    scanf("%i",&n1);
+   printf("Digite o valor do segundo numero:");
+    scanf("%i",&n2);
     if (ponteiro1>ponteiro2)
     {
-        printf("the bigger number is:%i\n",ponteiro1);
+        printf("O maior endereço de memória é %i pertence ao número %i\nE o menor endereço é %i pertence ao numero %i",ponteiro1,*ponteiro1,ponteiro2,*ponteiro2);
     }else{
-         printf("the bigger number is:%i\n",ponteiro2);
+        printf("O maior endereço de memória é %i pertence ao número %i\nE o menor endereço é %i pertence ao numero %i",ponteiro2,*ponteiro2,ponteiro1,*ponteiro1);
+ 
     }
 }
